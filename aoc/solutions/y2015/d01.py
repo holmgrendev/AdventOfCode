5b629afc06c4481bf51a76bc7897ea0104508c26
holmgrendev <alexander_holmgren@outlook.com>
2026-03-29 22:21:09 +0200

    Puzzle 2015-01 solved

--- aoc/solutions/y2015/d01.py
+++ aoc/solutions/y2015/d01.py
@@ -2,4 +2,17 @@ from aoc.puzzles import register
 
 @register(2015, 1, 1)
 def part1(input_data):
-    return "Initial tests"
+    floor = input_data.count("(") - input_data.count(")")
+    return str(floor)
+
+@register(2015, 1, 2)
+def part2(input_data):
+    floor = 0
+    for i, c in enumerate(input_data, start=1):
+        if c == "(":
+            floor += 1
+        else:
+            floor -= 1
+        
+        if floor < 0:
+            return i
